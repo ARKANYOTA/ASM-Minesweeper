f43f19ec9ea3a84377564c93efbfc1e34a4360e1
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2022-03-28 21:38:05 +0200

    Map more clean

--- main.asm
+++ main.asm
@@ -1,46 +1,55 @@
-section .data                           ;Data segment
-	LookUpDig         db "0123456789"             ; Translation Table
-	loseMsg           db 'Perdu Dommage'
-	lenloseMsg           equ $-loseMsg             ;The length of the message
-	posxMsg           db 'Position x: '
-	lenposxMsg           equ $-posxMsg             ;The length of the message
-	posyMsg           db 'Position y: '
-	lenposyMsg           equ $-posyMsg             ;The length of the message
-	bombs             DQ 0x0
-	flag              DQ 0x001
-	disco             DQ 0x33
-	vars              DQ 0
+section .data          ; Data segment
+	posMsg            db 'Position x et y sans separation: '
+	lenposxMsg           equ $-posMsg
+
+	youWinMessage     db 'You Win, Bravo!'
+	youLoseMessage    db 'You Lose, Ressaye!'
+	lenyouWinMessage     equ $-youWinMessage
+	lenyouLoseMessage    equ $-youLoseMessage
+
 	nb_bombs          DQ 14
+
+	bombs             DQ 0x0
+	flag              DQ 0x0
+	disco             DQ 0x0
+
 	zero              db "0"
 	uno               db "1"
-	bombsMsg          db "B "
-	nothingMsg        db ". "
 	sautdelMsg        db "",10
-	espace            db " "
 	whatCollIs        db "  0 1 2 3 4 5 6 7"
-	lenwhatCollIs        equ $-whatCollIs             ;The length of the message
+	lenwhatCollIs        equ $-whatCollIs
 
-section .bss           ;Uninitialized data
+	espace            db " "
+	noBombAndFlagMsg  db "~"
+	justbombMsg       db "#"
+	bombAndFlagMsg    db "x"
+
+	isDiscoveredMsg   db "-"
+	
+	isLose            DQ 0x0
+	isWin             DQ 0x0
+	
+
+section .bss           ; Uninitialized data
     cos resd 1
     x resb 1
     y resb 1
 
-section .text          ;Code Segment
+section .text          ; Code Segment
 	global _start
 
 generate_bombs:
-	
-	; rax = RANDOM 
-	L: rdrand ax    ; Générer un nombre aléatoire dans la variable eax 
-	jnc L           ; https://rosettacode.org/wiki/Random_number_generator_(device)#X86_Assembly
+	;rax = RANDOM 
+	L: rdrand ax        ; Générer un nombre aléatoire dans la variable eax 
+	jnc L               ; https://rosettacode.org/wiki/Random_number_generator_(device)#X86_Assembly
 
 	; rax = rax%64 
-	xor rdx, rdx     ; Reset la variale
-	mov rbx, 64     ; modulo 64 le nombre
-	div rbx         ; 
-	mov rax, rdx    ; Met le modulo dans la variable rax
+	xor rdx, rdx        ; Reset la variale
+	mov rbx, 64         ; modulo 64 le nombre
+	div rbx
+	mov rax, rdx        ; Met le modulo dans la variable rax
 
-	xor rdx, rdx    ; Reset rdx pour la prochaine utilisation
+	xor rdx, rdx        ; Reset rdx pour la prochaine utilisation
 	xor rbx, rbx
 
 	; Condition de si la bombe est deja placée
@@ -73,7 +82,7 @@ generate_bombs:
 
 affiche_grid:
 	; Verifie si c'est un modulo 8 pour sauter une ligne
-	mov rdx, 0      
+	xor rdx, rdx    
 	mov rax, rcx
 	mov rbx, 8
 	div rbx
@@ -108,27 +117,70 @@ affiche_grid:
 
 
 
+	;                     espace            db " "
+	;                     noBombAndFlagMsg  db "~"
+	;                     justbombMsg       db "#"
+	;                     bombAndFlag       db "x"
 	; Si le lsb modulo 2 est 1 ou 0 pour print 1 ou 0
 	mov rax, r8  ; Met r8(bombs avec le lsb etant la position acctuelle) dans rax
 	shr rax, 1   ; Passe a la bombe suivante
 	mov r8, rax  ; Re sauvgarde rax dans r8
 	push rcx     ; Sauvgarde le rcx pour pas le perdre
-	jc print_1
-	print_0:     ; Func Name... 
-		mov rax, 4
-		mov rbx, 1
-		mov rcx, nothingMsg 
-		mov rdx, 2
-		int 80h
-		; saut de ligne
-		jmp end_print
-	print_1:
-		mov rax, 4
-		mov rbx, 1
-		mov rcx, bombsMsg
-		mov rdx, 2
-		int 80h
+	jc print_bomb
+	print_no_bomb:     ; Func Name... 
+		mov rax, r9  ; Met r8(bombs avec le lsb etant la position acctuelle) dans rax
+		shr rax, 1   ; Passe a la bombe suivante
+		mov r9, rax  ; Re sauvgarde rax dans r8
+		jc print_no_bomb_and_flag
+			mov rax, 4
+			mov rbx, 1
+			mov rcx, espace
+			mov rdx, 1
+			int 80h
+			jmp end_print
+		print_no_bomb_and_flag:
+			mov rax, 4
+			mov rbx, 1
+			mov rcx, noBombAndFlagMsg 
+			mov rdx, 1
+			int 80h
+			jmp end_print
+	print_bomb:
+		mov rax, r9  ; Met r8(bombs avec le lsb etant la position acctuelle) dans rax
+		shr rax, 1   ; Passe a la bombe suivante
+		mov r9, rax  ; Re sauvgarde rax dans r8
+		jc print_bomb_and_flag
+			mov rax, 4
+			mov rbx, 1
+			mov rcx, justbombMsg
+			mov rdx, 1
+			int 80h
+			jmp end_print
+		print_bomb_and_flag:
+			mov rax, 4
+			mov rbx, 1
+			mov rcx, bombAndFlagMsg
+			mov rdx, 1
+			int 80h
+			jmp end_print
 	end_print:
+	mov rax, r10  ; Met r8(bombs avec le lsb etant la position acctuelle) dans rax
+	shr rax, 1   ; Passe a la bombe suivante
+	mov r10, rax  ; Re sauvgarde rax dans r8
+	jc print_is_disco
+		mov rax, 4
+		mov rbx, 1
+		mov rcx, espace 
+		mov rdx, 1
+		int 80h
+		jmp end_print_disco
+	print_is_disco:
+		mov rax, 4
+		mov rbx, 1
+		mov rcx, isDiscoveredMsg
+		mov rdx, 1
+		int 80h
+	end_print_disco:
 	pop rcx     ; Re recupere le rcx en tant que compteur 
 
 	dec rcx     ; Passe a l'itteration suivante
@@ -181,24 +233,20 @@ _start:                ;User prompt
     
     call user_input
 
-    mov rax, 0
+    mov rax, 0         ; Generere les bombes En fonction de l'input utilisateur (TODO)
     mov rbx, 64        ; Permet de faire un modulo 64
     mov rdx, 0 
     mov r8, [bombs]
     mov rcx, [nb_bombs]
     call generate_bombs
 
-    
 
-	; mov r, [bombs]
-	; call v2_print_bin                ; Affiche le nombre 103 en binaire
-
+; while_true:
     mov rax, 4
     mov rbx, 1
     mov rcx, whatCollIs
     mov rdx, lenwhatCollIs
     int 80h
-
 
     mov rax, 0
     mov rbx, 0
@@ -211,6 +259,37 @@ _start:                ;User prompt
     call affiche_grid
     mov r14, 0
 
+
+    mov r8, [bombs]
+    mov r9, [flag]
+    mov r10, [disco]
+   
+    xor rax, rax
+    xor rcx, rcx
+
+    
+    mov rbx, 1     ; Masque
+    mov rcx, rax
+    shl rbx, cl     ; masque = (1 << rax(position random de la bombe))
+    or r10, rbx    ; bombs |= masque
+    mov [disco], r10
+
+
+    mov r8, [bombs]
+    mov r9, [flag]
+    mov r10, [disco]
+    mov r15, 0               ; Is finished
+    
+   ;  not r11                  ; Si ~(disco) == bombs mettre r15 a 1
+   ;  cmp r11, r8
+   ;  je no_add_one_to_r15
+   ;      and r15, 1
+   ;  no_add_one_to_r15:
+
+
+   ;  cmp r15, 0
+   ;  je while_true
+
     ; Saut de ligne pour éviter le %
     mov rax, 4
     mov rbx, 1
@@ -218,7 +297,8 @@ _start:                ;User prompt
     mov rdx, 1
     int 80h
 
-	; exit(0)
-	mov     eax, 0x1              ; Set system_call
-	mov     ebx, 0               ; Exit_code 0
-	int     0x80                  ; Call kernel
+    ; exit(0)
+    mov     eax, 0x1              ; Set system_call
+    mov     ebx, 0               ; Exit_code 0
+    int     0x80                  ; Call kernel
+    
